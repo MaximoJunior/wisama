92fdd4b59bb429951cfdd6d4cbc5e8eb4e88b846
MaximoJunior <studentdeveloper19@gmail.com>
2021-11-25 11:33:06 -0500

    Done

--- js/index.js
+++ js/index.js
@@ -2,16 +2,16 @@
 
 const btnToggle = document.getElementById('btn-toggle');
 const navBar =  document.getElementById('main-nav-bar');
+const header =  document.getElementsByClassName('header')[0];
 
 btnToggle.addEventListener('click', function() {
 
     this.classList.toggle('btn-toggle-open');
     navBar.classList.toggle('nav-bar-close');
 
-})
+});
 
 // slider
-
 function sliderTimer() {
 
     var slider = document.getElementsByClassName('kv-slider')[0];
@@ -59,7 +59,7 @@ sliderTimer();
 
 
 function setBg(el) {
-    //    console.log(el);
+    
     var dataImage = el.getAttribute("data-image-name");
     dataImage = dataImage.split('.');
     var name = dataImage[0];
@@ -97,3 +97,25 @@ window.addEventListener('resize', function(){
   }
 
 });
+
+window.addEventListener("scroll", effectsHeader);
+
+//Header Effects
+function effectsHeader() {
+    var heigthHeader = header.offsetHeight;
+  
+     var distance = header.nextElementSibling.getBoundingClientRect().top;
+
+    if ( distance < heigthHeader) {
+        
+        if (header.classList.contains("header-bottom-shadow")) {
+            return;
+        }
+  
+        header.classList.add("header-bottom-shadow");
+  
+    }else{
+        header.classList.remove("header-bottom-shadow");
+    }
+    
+  }
