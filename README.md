ce4f28eec25f4fa77ecb2fb8a9f28e1fa5aaa373
MaximoJunior <studentdeveloper19@gmail.com>
2021-12-02 22:03:16 -0500

    New version - new Features added

--- README.md
+++
@@ -1,4 +0,0 @@
-# wisama 
-This website is in dev mode.\
-[Go to wisama](https://test1202.000webhostapp.com/).
-
---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="refresh" content="0;URL=https://misawa-kokoro.jp/">
+</head>
+<body>
+<?php
+header( "HTTP/1.1 301 Moved Permanently" ); 
+header( "Location: https://misawa-kokoro.jp/" ); 
+exit;
+?>
+</body>
+</html>
